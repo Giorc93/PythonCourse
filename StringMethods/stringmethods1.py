e5069cb0a7e2c86a91d6d364b356824ebae7baf9
Gio <example@test.com>
2020-05-21 16:56:10 -0500

    8th Commit: String methods

---
+++ StringMethods/stringmethods1.py
@@ -0,0 +1,13 @@
+upper()  # Uppercase
+lower()  # Lowercase
+capitalize()  # First letter on Uppercase
+count()  # Counts how many times the string or letter appears on a text or string
+find()  # Returns the index of the given string
+isdigit()  # Returns if the given value is numeric or not
+isalum()  # Returns if the given value is alphanumeric
+isalpha()  # Returns if the given value has only letter, doesn't count spaces
+split()  # Splits the words using spaces as separator
+strip()  # Removes the whitespaces on the begining and the end of the string
+replace()  # Replaces a word or a letter
+rfind()  # Reversed find
+...
