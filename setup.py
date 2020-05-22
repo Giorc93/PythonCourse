84886bd3fdbea2085a3b0edd3dd8b9403e12540d
Gio <example@test.com>
2020-05-21 19:16:17 -0500

    Dist Packages creation

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+
+    name='CalculatorPackage',
+    version='1.0',
+    description='Basic Caclculator',
+    author='Gio',
+    author_email='Gio@gio.com',
+    URL='www.gio.com.co',
+    packages=setuptools.find_packages()
+
+)
